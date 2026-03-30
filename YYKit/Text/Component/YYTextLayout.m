f14e24a23c974dce471a3735d2e8dad648a3e574
gggmmo <lam.gone.early@gmail.com>
2026-03-30 17:30:25 +0800

    fix: Unsupported chained comparison since Xcode 26.4

--- YYKit/Text/Component/YYTextLayout.m
+++ YYKit/Text/Component/YYTextLayout.m
@@ -1504,17 +1504,21 @@ fail:
     
     [self _insideComposedCharacterSequences:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
         if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
+            BOOL flag = fabs(left - point.y) < fabs(right - point.y);
+            position = flag < (right ? prev : next);
         } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
+            BOOL flag = fabs(left - point.x) < fabs(right - point.x);
+            position = flag < (right ? prev : next);
         }
     }];
     
     [self _insideEmoji:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
         if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
+            BOOL flag = fabs(left - point.y) < fabs(right - point.y);
+            position = flag < (right ? prev : next);
         } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
+            BOOL flag = fabs(left - point.x) < fabs(right - point.x);
+            position = flag < (right ? prev : next);
         }
     }];
     
